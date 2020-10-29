807953583d3b87628670159a4b4df7b2f9f9563a
Changxu Luo <changxu.luo@columbia.edu>
2020-10-29 12:18:50 -0400

    modified:   setup_scripts/setup_single_rocksdb.sh 	new file:   slf4j-simple-1.6.1.jar 	deleted:    ycsb_workloads/workload_1

--- setup_scripts/setup_single_rocksdb.sh
+++ setup_scripts/setup_single_rocksdb.sh
@@ -13,13 +13,28 @@ git clone https://github.com/brianfrankcooper/YCSB/
 cd YCSB/
 
 # Load the database
-./bin/ycsb load rocksdb -s -P ./workloads/workloada -p rocksdb.dir=/mnt/sdb/rocksdb/ -p \
-rocksdb.optionsfile=/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Scripts/rocksdb_config/rocksdb_2.ini \
+./bin/ycsb load rocksdb -s \
+-P /mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Scripts/ycsb_workloads/workload_16_50 \
+-p rocksdb.dir=/mnt/sdb/rocksdb/ -p \
+rocksdb.optionsfile=/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Scripts/rocksdb_config/rocksdb_1.ini \
 -threads 4 \
-> /mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Data/load_data_2.csv
+> /mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Data/load_data_1.csv
 
 # Run the experiment
-./bin/ycsb run rocksdb -s -P ./workloads/workloada -p rocksdb.dir=/mnt/sdb/rocksdb/ -p \
+./bin/ycsb run rocksdb -s \
+-P /mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Scripts/ycsb_workloads/workload_16_50 \
+-p rocksdb.optionsfile=/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Scripts/rocksdb_config/rocksdb_1.ini \
+-threads 16 -p \
+hdrhistogram.percentiles=50,90,95,99,99.9 \
+> /mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Data/run_data_1.csv
+
+# Load the database
+./bin/ycsb load rocksdb -s -P ./workloads/workloada -p rocksdb.dir=/mnt/sdb/my_rocksdb/ -p \
+rocksdb.optionsfile=/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Scripts/rocksdb_config/rocksdb_2.ini \
+-threads 4 \
+> /mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Data/load_data_3.csv
+
+./bin/ycsb run rocksdb -s -P ./workloads/workloada -p rocksdb.dir=/mnt/sdb/my_rocksdb/ -p \
 rocksdb.optionsfile=/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Scripts/rocksdb_config/rocksdb_2.ini \
 -threads 16 -p \
 hdrhistogram.percentiles=50,90,95,99,99.9 \
