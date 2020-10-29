ff283ed182d1a3fdec7d972aba8355b261bea77c
Changxu Luo <changxu.luo@columbia.edu>
2020-10-29 00:49:31 -0400

    deleted:    rocksdb.ini 	deleted:    setup.sh 	deleted:    setup_single_rdma.sh 	deleted:    setup_single_rocksdb.sh

--- setup.sh
+++
@@ -1,3 +0,0 @@
-sudo apt install default-jre
-
-./bin/ycsb load rocksdb -s -P workloads/workloada -p rocksdb.dir=~/rocksdb/ -p rocksdb.optionsfile=~/rocksdb_config/rocksdb.ini -threads 4
--- setup_single_rdma.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wget http://content.mellanox.com/ofed/MLNX_OFED-3.4-1.0.0.0/MLNX_OFED_LINUX-3.4-1.0.0.0-ubuntu14.04-x86_64.tgz
-tar -xvf MLNX_OFED_LINUX-3.4-1.0.0.0-ubuntu14.04-x86_64.tgz
-cd MLNX_OFED_LINUX-3.4-1.0.0.0-ubuntu14.04-x86_64/
-echo y | sudo ./mlnxofedinstall
-sudo /etc/init.d/openibd restart
-sudo /etc/init.d/opensmd restart
-sudo mst start
