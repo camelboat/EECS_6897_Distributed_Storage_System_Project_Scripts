d195a999c27087f8de08d7498520b5e9e2064033
Changxu Luo <changxu.luo@columbia.edu>
2020-11-04 21:44:33 -0500

    modified:   setup_scripts/run_ycsb.sh

--- setup_scripts/run_ycsb.sh
+++ setup_scripts/run_ycsb.sh
@@ -11,7 +11,7 @@ cd /mnt/sdb/YCSB/
 ./bin/ycsb run rocksdb -s \
 -P $WORKLOAD_FILE \
 -p rocksdb.dir=$ROCKSDB_DIR \
--p rocksdb.optionsfile=$CONFIGURATION_FILE \
+-p rocksdb.optionsfile=$ROCKSDB_CONFIG_FILE \
 -threads 16 \
 -p hdrhistogram.percentiles=50,90,95,99,99.9 \
 | tee $RUN_OUT_FILE;
