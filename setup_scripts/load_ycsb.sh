6fcf6e1583592e10ee4d4696b6daf813ed78131c
root <root@node0.lsm-1604-1.lsm-rep-pg0.cloudlab.umass.edu>
2020-11-10 00:53:59 -0500

    modified:   rocksdb_config/rocksdb_auto_compaction.ini 	modified:   rocksdb_config/rocksdb_no_auto_compaction.ini 	modified:   setup_scripts/load_ycsb.sh 	modified:   setup_scripts/run_ycsb.sh

--- setup_scripts/load_ycsb.sh
+++ setup_scripts/load_ycsb.sh
@@ -1,8 +1,8 @@
-WORKLOAD_FILE='/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Scripts/ycsb_workloads/workload_1-10_95-5'
-ROCKSDB_DIR='/mnt/sdb/archive_dbs/1-10_95-5/'
-LOAD_OUT_FILE='/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Data/load_1-10_95-5.csv'
+WORKLOAD_FILE='/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Scripts/ycsb_workloads/workload_16-50_95-5'
+ROCKSDB_DIR='/mnt/sdb/archive_dbs/16-50_95-5/'
+LOAD_OUT_FILE='/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Data/16-50_95-5/load_16-50_95-5.csv'
 ROCKSDB_CONFIG_FILE='/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Scripts/rocksdb_config/rocksdb_auto_compaction.ini'
-RUN_OUT_FILE='/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Data/1-10_95-5/run_1-10_95-5_auto.csv'
+RUN_OUT_FILE='/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Data/1-10_95-5/run_16-50_95-5_auto.csv'
 
 cd /mnt/sdb/YCSB/
 
--- setup_scripts/run_ycsb.sh
+++ setup_scripts/run_ycsb.sh
@@ -1,8 +1,8 @@
-WORKLOAD_FILE='/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Scripts/ycsb_workloads/workload_1-10_95-5'
-ROCKSDB_DIR='/mnt/sdb/archive_dbs/1-10_95-5/'
-LOAD_OUT_FILE='/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Data/load_1-10_95-5.csv'
-ROCKSDB_CONFIG_FILE='/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Scripts/rocksdb_config/rocksdb_auto_compaction.ini'
-RUN_OUT_FILE='/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Data/1-10_95-5/run_1-10_95-5_auto.csv'
+WORKLOAD_FILE='/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Scripts/ycsb_workloads/workload_16-50_95-5'
+ROCKSDB_DIR='/mnt/sdb/archive_dbs/16-50_95-5/'
+#ROCKSDB_CONFIG_FILE='/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Scripts/rocksdb_config/rocksdb_auto_compaction.ini'
+ROCKSDB_CONFIG_FILE='/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Scripts/rocksdb_config/rocksdb_no_auto_compaction.ini'
+RUN_OUT_FILE='/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Data/16-50_95-5/run_16-50_95-5_no.csv'
 
 # Make sure you copy both ROCKSDB_DIR and SST files dir before you run this script
 
