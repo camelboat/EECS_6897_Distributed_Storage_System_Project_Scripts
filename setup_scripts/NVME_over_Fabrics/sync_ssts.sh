9f9ca151597d820fe0b1742988ffb8e04497a263
Changxu Luo <changxu.luo@columbia.edu>
2020-12-03 02:39:31 -0500

    modified:   setup_scripts/NVME_over_Fabrics/sync_ssts.sh

--- setup_scripts/NVME_over_Fabrics/sync_ssts.sh
+++ setup_scripts/NVME_over_Fabrics/sync_ssts.sh
@@ -6,6 +6,22 @@ COMPACTION_META_PATH=/mnt/sdb/archive_dbs/compaction_meta/
 SST_PATH=/mnt/sdb/archive_dbs/sst_dir/sst_last_run/
 NVME_COMPACTION_META_PATH=/mnt/nvme0n1p4/
 
+function nvme_flush {
+  nvme flush /dev/nvme0n1p4 -n 10
+}
+
+# Arg1: File Path
+# Arg2: File Size
+function nvme_write {
+  # nvme write /dev/nvme0n1p4 -d $1 --data-size=$2
+  cp $1 "/mnt/nvme0n1p4/archive_dbs/sst_dir/sst_last_run/${1}"
+}
+
+# Arg1: File Path
+function nvme_delete {
+  rm "/mnt/nvme0n1p4/archive_dbs/sst_dir/sst_last_run/${1}"
+}
+
 inotifywait -m $COMPACTION_META_PATH -e create -e moved_to |
   while read path action file; do
     echo "${path} ${action} ${file}"
@@ -47,19 +63,3 @@ inotifywait -m $COMPACTION_META_PATH -e create -e moved_to |
     done < "$file_path"
     nvme_flush
   done
-
-function nvme_flush {
-  nvme flush /dev/nvme0n1p4 -n 10
-}
-
-# Arg1: File Path
-# Arg2: File Size
-function nvme_write {
-  # nvme write /dev/nvme0n1p4 -d $1 --data-size=$2
-  cp $1 "/mnt/nvme0n1p4/archive_dbs/sst_dir/sst_last_run/${1}"
-}
-
-# Arg1: File Path
-function nvme_delete {
-  rm "/mnt/nvme0n1p4/archive_dbs/sst_dir/sst_last_run/${1}"
-}
