781102c8355be2ba4d58f3f873c580a585808510
Changxu Luo <changxu.luo@columbia.edu>
2020-12-03 01:35:25 -0500

    modified:   setup_scripts/NVME_over_Fabrics/sync_ssts.sh

--- setup_scripts/NVME_over_Fabrics/sync_ssts.sh
+++ setup_scripts/NVME_over_Fabrics/sync_ssts.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-inotifywait -m /path -e create -e moved_to |
+# sudo apt install inotify-tools
+
+META_PATH=/mnt/sdb/archive_dbs/compaction_meta
+
+inotifywait -m $META_PATH -e create -e moved_to |
   while read path action file; do
     echo "${path} ${action} ${file}"
   done
