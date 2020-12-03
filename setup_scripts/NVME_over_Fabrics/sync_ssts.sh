ff9a7ab0415a0196b06d71d6c91744c0cf558c93
Changxu Luo <changxu.luo@columbia.edu>
2020-12-03 02:38:18 -0500

    modified:   setup_scripts/NVME_over_Fabrics/sync_ssts.sh

--- setup_scripts/NVME_over_Fabrics/sync_ssts.sh
+++ setup_scripts/NVME_over_Fabrics/sync_ssts.sh
@@ -4,6 +4,7 @@
 
 COMPACTION_META_PATH=/mnt/sdb/archive_dbs/compaction_meta/
 SST_PATH=/mnt/sdb/archive_dbs/sst_dir/sst_last_run/
+NVME_COMPACTION_META_PATH=/mnt/nvme0n1p4/
 
 inotifywait -m $COMPACTION_META_PATH -e create -e moved_to |
   while read path action file; do
@@ -12,14 +13,53 @@ inotifywait -m $COMPACTION_META_PATH -e create -e moved_to |
     line_num=0
     while IFS= read -r line; do
       # echo "$line"
-      if [ $line_num == 0 ]; then echo "First line, need to delete"; fi
-      if [ $line_num == 1 ]; then echo "Second line, need to delete"; fi
-      if [ $line_num == 2 ]; then echo "Third line, need to write and flush"; fi
+      if [ $line_num == 0 ]; then
+        for word in $line; do
+          if [ $word_num -ne 0 ]; then
+            echo "delete $word";
+            nvme_delete $file_path;
+          fi
+          word_num=$(($word_num+1))
+        done
+      elif [ $line_num == 1 ]; then
+        echo "Second line, need to delete";
+        for word in $line; do
+          if [ $word_num -ne 0 ]; then
+            echo "delete $word";
+            nvme_delete $file_path
+          fi
+          word_num=$(($word_num+1))
+        done
+      elif [ $line_num == 2 ]; then
+        echo "Third line, need to write and flush";
+        for word in $line; do
+          if [ $word_num -ne 0 ]; then
+            echo "write $word";
+            data_size=$(wc -c < $file_path)
+            nvme_write $file_path $data_size
+          fi
+          word_num=$(($word_num+1))
+        done
+      fi
       word_num=0
-      for word in $line; do
-        if [ $word_num -ne 0 ]; then echo $word; fi
-        word_num=$(($word_num+1))
-      done
+
       line_num=$(($line_num+1))
     done < "$file_path"
+    nvme_flush
   done
+
+function nvme_flush {
+  nvme flush /dev/nvme0n1p4 -n 10
+}
+
+# Arg1: File Path
+# Arg2: File Size
+function nvme_write {
+  # nvme write /dev/nvme0n1p4 -d $1 --data-size=$2
+  cp $1 "/mnt/nvme0n1p4/archive_dbs/sst_dir/sst_last_run/${1}"
+}
+
+# Arg1: File Path
+function nvme_delete {
+  rm "/mnt/nvme0n1p4/archive_dbs/sst_dir/sst_last_run/${1}"
+}
