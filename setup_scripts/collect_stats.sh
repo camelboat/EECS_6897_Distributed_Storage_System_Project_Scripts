d523d2ac8f8846c196bdddf0208948d659c2a9e4
Changxu Luo <changxu.luo@columbia.edu>
2020-12-08 00:49:41 -0500

    modified:   setup_scripts/collect_stats.sh

--- setup_scripts/collect_stats.sh
+++ setup_scripts/collect_stats.sh
@@ -4,7 +4,7 @@ OUTPUT_PATH="/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Data/report"
 EXPERIMENT_NAME="load_base"
 
 while true; do
-  top -b -n 1 | grep java | tee -a ${OUPUT_PATH}/${EXPERIMENT_NAME}_top.csv;
+  top -b -n 1 | grep java | tee -a ${OUTPUT_PATH}/${EXPERIMENT_NAME}_top.csv;
   iostat | tee -a ${OUTPUT_PATH}/${EXPERIMENT_NAME}_iostat.csv;
   sleep 5;
 done
