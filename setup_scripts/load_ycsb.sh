6152118a24390493dcbc43ec4119002c76a761fd
changxu <camelboat@sjtu.edu.cn>
2020-12-09 17:57:06 -0500

    create function create_or_move

--- setup_scripts/load_ycsb.sh
+++ setup_scripts/load_ycsb.sh
@@ -4,6 +4,8 @@ WORKLOAD_NUM='16-50_95-5'
 #WORKLOAD_NUM='16-50_100-0'
 WORKLOAD_FILE="/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Scripts/ycsb_workloads/workload_${WORKLOAD_NUM}"
 ROCKSDB_DIR="/mnt/sdb/archive_dbs/${WORKLOAD_NUM}"
+COMPACTION_META_PATH="/mnt/sdb/archive_dbs/compaction_meta"
+MANIFEST_META_PATH="/mnt/sdb/archive_dbs/manifest_meta"
 # LOAD_OUT_FILE="/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Data/${WORKLOAD_NUM}/load_${WORKLOAD_NUM}.csv"
 LOAD_OUT_FILE="/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Data/report/load_base_${WORKLOAD_NUM}.csv"
 ROCKSDB_CONFIG_FILE='/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Scripts/rocksdb_config/rocksdb_auto_compaction_16.ini'
@@ -17,22 +19,26 @@ REMOTE_SST_WORK_DIR="/mnt/nvme0n1p4/archive_dbs/sst_dir/sst_last_run"
 rm ${REMOTE_SST_WORK_DIR}/*
 nvme flush /dev/nvme0n1p4 -n 10
 
+function create_or_remove {
+    if [ -d $1 ]; then
+        rm -rf $1
+    fi
+    mkdir -p $1
+}
+
 cd /mnt/sdb/YCSB/
 
-echo "remove rocksdb working dir"
-if [ -d $ROCKSDB_DIR ]; then
-    rm -rf $ROCKSDB_DIR
-else
-    mkdir -p $ROCKSDB_DIR
-fi
+echo "create or remove rocksdb working dir"
+create_or_remove $ROCKSDB_DIR
 
-echo "finished, then remove sst dir"
-if [ -d $SST_WORK_DIR ]; then
-    mkdir -p $SST_WORK_DIR
-else
-    rm -rf $SST_WORK_DIR
-fi
-echo "finished"
+echo "create of remove sst work dir"
+create_or_remove $SST_WORK_DIR
+
+echo "create or remove compaction meta folder"
+create_or_remove $COMPACTION_META_PATH
+
+echo "create or remove manifest meta folder"
+create_or_remove $MANIFEST_META_PATH
 
 cgexec -g memory:mlsm \
 ./bin/ycsb load rocksdb -s \
