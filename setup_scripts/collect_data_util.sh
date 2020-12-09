d72cb8843bc4be717ba98170ca7b775626876828
changxu <camelboat@sjtu.edu.cn>
2020-12-09 01:12:04 -0500

    modified:   setup_scripts/collect_data_util.sh 	modified:   setup_scripts/collect_stats.sh

--- setup_scripts/collect_data_util.sh
+++ setup_scripts/collect_data_util.sh
@@ -10,8 +10,12 @@ iostat -p sda 2 6
 OUTPUT_PATH="/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Data/report"
 EXPERIMENT_NAME="load_base"
 
+JAVA_PID=$(top -b -n 1 | grep top | sed '3!d' | awk '{print $1}')
+echo "RocksDB PID: ${JAVA_PID}"
+
 while true; do
-  top -b -n 1 | grep java | tee -a ${OUPUT_PATH}/${EXPERIMENT_NAME}_top.csv;
-  iostat | tee -a ${OUTPUT_PATH}/${EXPERIMENT_NAME}_iostat.csv;
+  # top -b -n 1 | grep java | tee -a ${OUPUT_PATH}/${EXPERIMENT_NAME}_top.csv;
+  ps -p $JAVA_PID -o %cpu,%mem,cmd | tee -a {OUPUT_PATH}/${EXPERIMENT_NAME}_top.csv
+  iostat | sed '7!d' | tee -a ${OUTPUT_PATH}/${EXPERIMENT_NAME}_iostat.csv;
   sleep 5;
 done
--- setup_scripts/collect_stats.sh
+++ setup_scripts/collect_stats.sh
@@ -3,13 +3,17 @@
 OUTPUT_PATH="/mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Data/report"
 EXPERIMENT_NAME="load_base"
 
+JAVA_PID=$(top -b -n 1 | grep top | sed '3!d' | awk '{print $1}')
+echo "RocksDB PID: ${JAVA_PID}"
+
 while true; do
-  iostat | tee -a ${OUTPUT_PATH}/${EXPERIMENT_NAME}_iostat.csv;
+  iostat | tee -a | sed '7!d'  ${OUTPUT_PATH}/${EXPERIMENT_NAME}_iostat.csv;
   tmp=$(top -b -n 1 | grep java)
   if [ -z "$tmp" ]; then
     break
   else
-    echo $tmp >> ${OUTPUT_PATH}/${EXPERIMENT_NAME}_top.csv
+    # echo $tmp >> ${OUTPUT_PATH}/${EXPERIMENT_NAME}_top.csv
+    ps -p $JAVA_PID -o %cpu,%mem,cmd | tee -a {OUPUT_PATH}/${EXPERIMENT_NAME}_ps.csv
   fi
   # top -b -n 1 | grep java | tee -a ${OUTPUT_PATH}/${EXPERIMENT_NAME}_top.csv;
   sleep 5;
