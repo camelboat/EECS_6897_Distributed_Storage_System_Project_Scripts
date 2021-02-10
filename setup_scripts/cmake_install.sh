b87acc2252099e8dbb86391d21c4291f6e4720fd
Changxu Luo <changxu.luo@columbia.edu>
2021-02-09 22:39:33 -0500

    Script to update cmake version

---
+++ setup_scripts/cmake_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+nproc=16
+
+#remove the default version
+sudo apt remove --purge --auto-remove cmake
+
+version=3.19
+build=1
+mkdir ~/temp
+cd ~/temp
+wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
+tar -xzvf cmake-$version.$build.tar.gz
+
+cd cmake-$version.$build/
+./bootstrap
+make -j$(nproc)
+sudo make install
+
+cd ~
+rm -rf ./temp
