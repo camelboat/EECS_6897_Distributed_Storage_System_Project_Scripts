308a46932e0f472319c458bc622d1292e1070588
axe <xz2872@columbia.edu>
2021-02-28 04:22:08 -0700

    jsonify version edit

--- version_edit_sync/cpp/rubble_server.h
+++ version_edit_sync/cpp/rubble_server.h
@@ -2,7 +2,10 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>   
 #include <memory>
+#include <typeinfo> 
+#include <nlohmann/json.hpp>
 
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/health_check_service_interface.h>
@@ -16,7 +19,6 @@
 #include "db/db_impl/db_impl.h"
 #include "rocksdb/slice.h"
 #include "rocksdb/options.h"
-
 
 using grpc::Server;
 using grpc::ServerBuilder;
@@ -34,27 +36,205 @@ using version_edit_sync::GetRequest;
 using version_edit_sync::PutReply;
 using version_edit_sync::PutRequest;
 
+using version_edit_sync::DeleteFile;
+using version_edit_sync::NewFile;
+using version_edit_sync::NewFile_FileMetaData;
+using version_edit_sync::NewFile_FileMetaData_FileDescriptor;
+using version_edit_sync::VersionEditToSync;
+
+using json = nlohmann::json;
+
 class VersionEditSyncServiceImpl final : public VersionEditSyncService::Service {
   public:
     explicit VersionEditSyncServiceImpl(rocksdb::DB* db)
-      :db_(db),put_count_(0){};
+      :db_(db),put_count_(0),log_apply_counter_(0){};
     
     ~VersionEditSyncServiceImpl(){
       delete db_;
     }
 
+  void PopulateVersionEdit(const VersionEditToSync& v_edit, rocksdb::VersionEdit* edit){
+      
+      edit->SetLogNumber(v_edit.log_number());
+      edit->SetPrevLogNumber(v_edit.prev_log_number());
+      edit->SetColumnFamily(v_edit.column_family());
+
+      // add deletions to edit
+      for(int i = 0; i < v_edit.del_size(); i++){
+        DeleteFile del = v_edit.del(i);
+        edit->DeleteFile(del.level(), del.file_number());
+      }
+
+      // add newFile to edit
+      for(int i = 0; i < v_edit.new__size(); i++){
+        NewFile add = v_edit.new_(i);
+
+        NewFile_FileMetaData meta = add.meta();
+        NewFile_FileMetaData_FileDescriptor fd = meta.fd();
+
+        rocksdb::InternalKey smallest;
+        std::string rep = meta.smallest_key();
+        rocksdb::ParsedInternalKey parsed;
+        if (rocksdb::ParseInternalKey(rocksdb::Slice(rep), &parsed) == rocksdb::Status::OK()){
+            smallest.SetFrom(parsed);
+        }
+
+        rocksdb::InternalKey largest;
+        rep = meta.largest_key();
+        if(rocksdb::ParseInternalKey(rocksdb::Slice(rep),  &parsed) == rocksdb::Status::OK()){
+          largest.SetFrom(parsed);
+        }
+
+        edit->AddFile(add.level(), fd.file_number(), 0 /* path_id shoule be 0*/,
+                      fd.file_size(), 
+                      smallest, largest, 
+                      fd.smallest_seqno(), fd.largest_seqno(),
+                      false, 
+                      rocksdb::kInvalidBlobFileNumber,
+                      meta.oldest_ancestor_time(),
+                      meta.file_creation_time(),
+                      meta.file_checksum(), 
+                      meta.file_checksum_func_name()
+                      );
+      }
+    }
+
+  void ParseJsonStringToVersionEdit(const std::string& edit_json, rocksdb::VersionEdit* edit){
+
+      std::cout << " ---------- calling ParseJsonStringToVersionEdit ----------- \n";
+      auto j = json::parse(edit_json);
+
+      std::cout << "Dumped Json VersionEdit : " << j.dump(4) << std::endl;
+      if(!j["LogNumber"].is_null()){
+        edit->SetLogNumber(j["LogNumber"].get<uint64_t>());
+        // std::cout << " LogNumber : " << j["LogNumber"].get<uint64_t>() << std::endl;
+      }
+
+      if(!j["PrevLogNumber"].is_null()){
+        edit->SetPrevLogNumber(j["PrevLogNumber"].get<uint64_t>());
+        // std::cout << " PrevLogNumver " << j["PrevLogNumber"].get<uint64_t>() << std::endl;
+      }
+      assert(!j["ColumnFamily"].is_null());
+      edit->SetColumnFamily(j["ColumnFamily"].get<uint32_t>());
+
+      for(auto j_added_file : j["AddedFiles"].get<std::vector<json>>()){
+          // std::cout << " Added File : " << j_added_file << std::endl;
+          assert(!j_added_file["SmallestUserKey"].is_null());
+          assert(!j_added_file["SmallestSeqno"].is_null());
+          rocksdb::InternalKey smallest(rocksdb::Slice(j_added_file["SmallestUserKey"].get<std::string>()), 
+                                          j_added_file["SmallestSeqno"].get<uint64_t>(),rocksdb::ValueType::kTypeValue);
+
+          assert(smallest.Valid());
+          std::string* rep = smallest.rep();
+          std::cout << "Smallest InternalKey rep : " << rep << std::endl;
+
+          uint64_t smallest_seqno = j_added_file["SmallestSeqno"].get<uint64_t>();
+         
+          std::cout <<"Smallest IKey : " <<  smallest.DebugString(false) << std::endl;
+          std::cout << "Smallest Seqno : " << smallest_seqno << std::endl;
+
+          assert(!j_added_file["LargestUserKey"].is_null());
+          assert(!j_added_file["LargestSeqno"].is_null());
+          rocksdb::InternalKey largest(rocksdb::Slice(j_added_file["LargestUserKey"].get<std::string>()), 
+                                          j_added_file["LargestSeqno"].get<uint64_t>(),rocksdb::ValueType::kTypeValue);
+
+          assert(largest.Valid());
+          rep = largest.rep();
+          std::cout << "Largest InternalKey rep : " << rep << std::endl;
+
+          uint64_t largest_seqno = j_added_file["LargestSeqno"].get<uint64_t>();
+
+          std::cout <<"Largest IKey : " <<  largest.DebugString(false) << std::endl;
+          std::cout << "Largest Seqno : " << largest_seqno << std::endl;
+
+          edit->AddFile(j_added_file["Level"].get<int>(), j_added_file["FileNumber"].get<uint64_t>(), 0 /* path_id shoule be 0*/,
+                      j_added_file["FileSize"].get<uint64_t>(), 
+                      smallest, largest, 
+                      smallest_seqno, largest_seqno,
+                      false, 
+                      rocksdb::kInvalidBlobFileNumber,
+                      rocksdb::kUnknownOldestAncesterTime,
+                      rocksdb::kUnknownFileCreationTime,
+                      rocksdb::kUnknownFileChecksum, 
+                      rocksdb::kUnknownFileChecksumFuncName
+                      );
+      }
+
+      if(!j["DeletedFiles"].is_null()){
+        // std::cout << " DeletedFiles : " << j["DeletedFiles"].get<std::vector<json>>() << std::endl;
+        for(auto j_delete_file : j["DeletedFiles"].get<std::vector<json>>()){
+          edit->DeleteFile(j_delete_file["Level"].get<int>(), j_delete_file["FileNumber"].get<uint64_t>());
+        }
+      }
+
+  }
+
+  void DebugJsonString(const std::string& edit_json){
+
+      bool debug = false;
+      if(debug) {
+        auto j = json::parse(edit_json);
+        std::cout << "Dumped Json VersionEdit : " << j.dump(4) << std::endl;
+        if(!j["PrevLogNumber"].is_null()){
+           std::cout << " PrevLogNumver " << j["PrevLogNumber"].get<uint64_t>() << std::endl;
+        }
+        if(!j["LogNumber"].is_null()){
+           std::cout << " LogNumber : " << j["LogNumber"].get<uint64_t>() << std::endl;
+        }
+
+        std::cout << " AddedFiles : " << j["AddedFiles"].get<std::vector<json>>() << std::endl;
+
+        if(!j["DeletedFiles"].is_null()){
+          std::cout << " DeletedFiles : " << j["DeletedFiles"].get<std::vector<json>>() << std::endl;
+        }
+      }
+  }
+
   Status VersionEditSync(ServerContext* context, const VersionEditSyncRequest* request, 
                           VersionEditSyncReply* reply) override {
-    // std::string prefix("Received record ");
-    // reply->set_message(prefix+request->record());
-    std::cout << "[Received record] : " << request->record() << "\n";
+    log_apply_counter_++;
+    std::cout << " ------------- Calling LogApply " << log_apply_counter_.load() << " th times ------------- \n";
+    rocksdb::VersionEdit edit;
+    /**
+     * example version_edit json: 
+     *  {
+     *    "AddedFiles": [
+     *         {
+     *            "FileNumber": 12,
+     *            "FileSize": 71819,
+     *            "LargestIKey": "'key7172' seq:7173, type:1",
+     *            "LargestSeqno": 7173,
+     *            "LargestUserKey": "key7172",
+     *            "Level": 0,
+     *            "SmallestIKey": "'key3606' seq:3607, type:1",
+     *            "SmallestSeqno": 3607,
+     *            "SmallestUserKey": "key3606"
+     *         }
+     *     ],
+     *     "ColumnFamily": 0,
+     *     "DeletedFiles": [
+     *         {
+     *             "FileNumber": 8,
+     *             "Level": 0
+     *         },
+     *         {
+     *             "FileNumber": 12,
+     *             "Level": 0
+     *         }
+     *      ],
+     *     "EditNumber": 2,
+     *     "LogNumber": 11,
+     *     "PrevLogNumber": 0
+     *   }
+     */ 
 
-    rocksdb::VersionEdit v_edit;
-    rocksdb::Status s = v_edit.DecodeFrom(rocksdb::Slice(request->record()));
-    std::cout << s.ToString() << std::endl;
-    // assert(s.ok());
+    std::string edit_json = request->edit_json();
 
-    std::cout << v_edit.DebugString(true) << std::endl;
+    DebugJsonString(edit_json);
+    ParseJsonStringToVersionEdit(edit_json, &edit);
+
+    rocksdb::Status s;
+    std::cout << "ReConstructed VersionEdit : " << edit.DebugString(true) << std::endl;
 
     rocksdb::DBImpl* impl_ = (rocksdb::DBImpl*)db_;
     
@@ -62,7 +242,7 @@ class VersionEditSyncServiceImpl final : public VersionEditSyncService::Service 
     rocksdb::InstrumentedMutex* mu = impl_->mutex();
     rocksdb::InstrumentedMutexLock l(mu);
     rocksdb::VersionSet* version_set = impl_->TEST_GetVersionSet();
-    // rocksdb::VersionSet* version_set = impl_->versions_.get();
+
     rocksdb::ColumnFamilyData* default_cf = version_set->GetColumnFamilySet()->GetDefault();
     const rocksdb::MutableCFOptions* cf_options = default_cf->GetLatestMutableCFOptions();
 
@@ -73,7 +253,7 @@ class VersionEditSyncServiceImpl final : public VersionEditSyncService::Service 
     mutable_cf_options_list.emplace_back(cf_options);
 
     rocksdb::autovector<rocksdb::VersionEdit*> edit_list;
-    edit_list.emplace_back(&v_edit);
+    edit_list.emplace_back(&edit);
 
     rocksdb::autovector<rocksdb::autovector<rocksdb::VersionEdit*>> edit_lists;
     edit_lists.emplace_back(edit_list);
@@ -95,7 +275,7 @@ class VersionEditSyncServiceImpl final : public VersionEditSyncService::Service 
     auto vstorage = default_cf->current()->storage_info();
     const char* c = vstorage->LevelSummary(&tmp);
 
-    std::cout << std::string(c) << std::endl;
+    std::cout << " VersionStorageInfo->LevelSummary : " << std::string(c) << std::endl;
     return Status::OK;
 
   }
@@ -140,7 +320,7 @@ class VersionEditSyncServiceImpl final : public VersionEditSyncService::Service 
           std::cout << "caliing put : (" << key << "," << value <<")\n";  
         }
         rocksdb::Status s = db_->Put(rocksdb::WriteOptions(), key, value);
-        std::cout << "Put " << put_count_ << " status: " << s.ToString();
+        // std::cout << "Put " << put_count_ << " status: " << s.ToString() << std::endl;
         if (s.ok()){
             reply.set_ok(true);
         }else{
@@ -156,7 +336,7 @@ class VersionEditSyncServiceImpl final : public VersionEditSyncService::Service 
   private:
     rocksdb::DB* db_;
     std::atomic<uint64_t> put_count_;
-
+    std::atomic<uint64_t> log_apply_counter_;
 };
 
 void RunServer(rocksdb::DB* db, const std::string server_addr) {
