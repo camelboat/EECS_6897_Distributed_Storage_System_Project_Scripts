130da1e506a98afa7f196cb0d4182276f7136542
axe <xz2872@columbia.edu>
2021-03-03 21:23:27 -0700

    Get test succeeds

--- version_edit_sync/cpp/kv_store_client.cc
+++ version_edit_sync/cpp/kv_store_client.cc
@@ -2,17 +2,18 @@
 
 int main(){
 
-// sending kv to ptimary
-  RubbleClient client1(grpc::CreateChannel(
+// client used to send kv to ptimary
+  KvStoreClient client1(grpc::CreateChannel(
     "localhost:50051", grpc::InsecureChannelCredentials()), true);
 // sending kv to the secondary
-  RubbleClient client2(grpc::CreateChannel(
+  KvStoreClient client2(grpc::CreateChannel(
     "localhost:50050", grpc::InsecureChannelCredentials()), false);
 
   grpc::Status s;
   std::vector<std::pair<std::string, std::string>> kvs;
 
-  for (int i = 0; i < 50000; i++){
+  int num_of_kv = 100000;
+  for (int i = 0; i < num_of_kv; i++){
       //sending the kv pairs to both primary and secondary server
       s = client1.Put(std::pair<std::string, std::string>("key" + std::to_string(i), "val" + std::to_string(i)));
       assert(s.ok());
@@ -21,7 +22,7 @@ int main(){
   }
   
   std::vector<std::string> keys;
-  for(int i = 0; i <50000; i++){
+  for(int i = 0; i <num_of_kv; i++){
     keys.emplace_back("key" + std::to_string(i));
   }
 
@@ -30,12 +31,13 @@ int main(){
   for(int i = 0 ; i < vals.size(); i++){
     assert(vals[i] == ("val" + std::to_string(i)));
   }
+
   vals.erase(vals.begin(), vals.end());
   client2.Get(keys, vals);
   for(int i = 0 ; i < vals.size(); i++){
     assert(vals[i] == ("val" + std::to_string(i)));
   }
 
-
+ 
   return 0;
 }
