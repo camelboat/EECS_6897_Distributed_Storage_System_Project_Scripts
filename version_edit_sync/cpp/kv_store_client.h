a36c7ffb0d342193ec8ad5266a7b1c95ec1547bb
axe <xz2872@columbia.edu>
2021-03-03 21:23:09 -0700

    separate sync_client and kv_client from rubble_client

--- version_edit_sync/cpp/kv_store_client.h
+++ version_edit_sync/cpp/kv_store_client.h
@@ -4,38 +4,36 @@
 #include <iostream>
 #include <string>
 
-// #include <grpcpp/grpcpp.h>
-// #include <grpcpp/health_check_service_interface.h>
-// #include <grpcpp/ext/proto_server_reflection_plugin.h>
-// #include "rubble_kv_store.grpc.pb.h"
-#include "rubble_client.h"
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include "rubble_kv_store.grpc.pb.h"
 
 using grpc::Channel;
 using grpc::ClientContext;
 using grpc::Status;
-using rubble::RubbleKvStoreService;
-using rubble::SyncRequest;
-using rubble::SyncReply;
 
+using rubble::RubbleKvStoreService;
 using rubble::GetReply;
 using rubble::GetRequest;
 using rubble::PutReply;
 using rubble::PutRequest;
 
-// class KvStoreClient{
+// Client used by user to make Put/Get requests to the db server
+class KvStoreClient{
 
-  // public:
-  //   KvStoreClient(std::shared_ptr<Channel> channel)
-  //       : stub_(RubbleKvStoreService::NewStub(channel)),
-  //       to_primary_(false){};
+  public:
+    KvStoreClient(std::shared_ptr<Channel> channel)
+        : stub_(RubbleKvStoreService::NewStub(channel)),
+        to_primary_(false){};
 
-  //   KvStoreClient(std::shared_ptr<Channel> channel, bool to_primary)
-  //       : stub_(RubbleKvStoreService::NewStub(channel)),
-  //       to_primary_(to_primary){};
+    KvStoreClient(std::shared_ptr<Channel> channel, bool to_primary)
+        : stub_(RubbleKvStoreService::NewStub(channel)),
+        to_primary_(to_primary){};
     
   // Requests each key in the vector and displays the key and its corresponding
   // value as a pair
-  Status RubbleClient::Get(const std::vector<std::string>& keys, std::vector<std::string>& vals) {
+  Status Get(const std::vector<std::string>& keys, std::vector<std::string>& vals) {
     // Context for the client. It could be used to convey extra information to
     // the server and/or tweak certain RPC behaviors.
     ClientContext context;
@@ -64,7 +62,7 @@ using rubble::PutRequest;
     return status;
   }
 
-  Status RubbleClient::Put(const std::pair<std::string, std::string>& kv){
+  Status Put(const std::pair<std::string, std::string>& kv){
     ClientContext context;
     auto stream = stub_->Put(&context);
     // for(const auto& kv: kvs){
@@ -108,10 +106,10 @@ using rubble::PutRequest;
     return s;
   }
 
-  // private:
-  //   std::unique_ptr<RubbleKvStoreService::Stub> stub_ = nullptr;
-  //     // Is this client sending kv to the primary? If not, it's sending kv to the secondary
-  //   bool to_primary_ = false;
-  //   std::atomic<uint64_t> put_count_{0};
-// };
+  private:
+    std::unique_ptr<RubbleKvStoreService::Stub> stub_ = nullptr;
+      // Is this client sending kv to the primary? If not, it's sending kv to the secondary
+    bool to_primary_ = false;
+    std::atomic<uint64_t> put_count_{0};
+};
 
