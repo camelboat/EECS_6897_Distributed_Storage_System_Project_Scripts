1376a91d16ec61608091c2a75016104684f2673a
axe <axe@node0.axe-qv93794.lsm-rep-pg0.utah.cloudlab.us>
2021-03-13 22:30:20 -0700

    modified: setup_single_env.sh

--- setup_scripts/setup_single_env.sh
+++ setup_scripts/setup_single_env.sh
@@ -16,17 +16,13 @@
 # The system by default would set /dev/nvme0n1p3 as the swap space, and we need to turn it off by:
 # swapoff /dev/nvme0n1p3
 
-BLOCK_DEVICE="nvme0n1p4"
+BLOCK_DEVICE="sdb"
 
 # Mount the disk /dev/sdb to /mnt/sdb for more disk spaces
 echo y | sudo mkfs.ext4 /dev/$BLOCK_DEVICE
 sudo mkdir /mnt/sdb
 sudo mount /dev/$BLOCK_DEVICE /mnt/sdb
 # Then run everything under /mnt/sdb
-
-# Vimrc
-cd /root/
-wget https://gist.githubusercontent.com/simonista/8703722/raw/d08f2b4dc10452b97d3ca15386e9eed457a53c61/.vimrc
 
 sudo apt update
 
@@ -40,31 +36,4 @@ echo y | sudo apt install maven
 # Install cgroup tools
 echo y | sudo apt install cgroup-tools
 
-# Install inotify-tools
-echo y | sudo apt install inotify-tools
 
-# Install sysstat for iostat
-echo y | sudo apt install sysstat
-
-# Install python3 virtualenv
-echo y | sudo apt install python3-venv python-dev
-
-# Clone scripts and data
-cd /mnt/sdb
-git clone https://github.com/camelboat/EECS_6897_Distributed_Storage_System_Project_Scripts.git
-git clone https://github.com/camelboat/EECS_6897_Distributed_Storage_System_Project_Data.git
-
-sudo mkdir -p /mnt/sdb/archive_dbs/sst_dir/sst_last_run
-sudo mkdir -p /mnt/sdb/archive_dbs/compaction_meta
-sudo mkdir -p /mnt/sdb/archive_dbs/manifest_meta
-
-cd /mnt/sdb
-git clone https://github.com/brianfrankcooper/YCSB
-cd /mnt/sdb/YCSB
-mvn -pl com.yahoo.ycsb:rocksdb-binding -am clean package
-
-# For remote editor client.
-chown -R cl3875 /mnt
-
-# Check space of the disk where current path is
-df -Ph . | tail -1 | awk '{print $4}'
