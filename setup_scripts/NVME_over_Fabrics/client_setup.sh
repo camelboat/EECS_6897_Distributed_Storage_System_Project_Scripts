19c4dbc40b96712390f9c13943bffcad25b2867a
Changxu <>
2021-04-03 18:45:59 -0400

    nit

--- setup_scripts/NVME_over_Fabrics/client_setup.sh
+++ setup_scripts/NVME_over_Fabrics/client_setup.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
+set -ex
+
+# NVMe over RoCE setup for client side
 # Client should be the one who visits nvme device on other nodes(targets) 
 
-#NVMe over RoCE setup for client side
 modprobe nvme-rdma
 
-sudo apt install uuid-dev
+echo 'y' | sudo apt install uuid-dev
 
 # git clone https://github.com/linux-nvme/nvme-cli.git
-
 # cd nvme-cli
 # make
 # make install
 
-sudo apt install nvme-cli
+echo 'y' | sudo apt install nvme-cli
 
 nvme gen-hostnqn > /etc/nvme/hostnqn
