e4fd4345905c8f4fc888f34b97adc5acbda6e771
Changxu <cl3875@columbia.edu>
2021-04-04 07:36:50 -0600

    Allow target ip address as an argument

--- setup_scripts/NVME_over_Fabrics/target_setup.sh
+++ setup_scripts/NVME_over_Fabrics/target_setup.sh
@@ -1,49 +1,62 @@
 #!/usr/bin/env bash
 
+set -e
+
 # target server setup to enable NVMe over RoCE
 
 #echo y | sudo mkfs.ext4 /dev/nvme0n1p4 # This is the part name for m510
 #sudo mkdir /mnt/nvme0n1p4
 #sudo mount /dev/nvme0n1p4 /mnt/nvme0n1p4
 
-
-#set this to the ip addr of the target machine
 IP_ADDR=10.10.1.2
+DEVICE_PATH=/dev/nvme0n1
 
-echo "executing: modprobe nvmet"
+for i in "$@"
+do
+case $i in
+    -a=*|--target-ip-address=*)
+    IP_ADDR="${i#*=}"
+    shift # past argument=value
+    ;;
+    -d=*|--device_path=*)
+    DEVICE_PATH="${i#*=}"
+    shift
+    ;;
+    --default)
+    DEFAULT=YES
+    shift # past argument with no value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+
 modprobe nvmet
 
-echo "executing: modprobe nvmet-rdma"
 modprobe nvmet-rdma
 
-echo "executing: configure nvmet subsystems"
 # create a nvme subsystem called "nvme-target1"
-mkdir /sys/kernel/config/nvmet/subsystems/nvme-target1
+mkdir -p /sys/kernel/config/nvmet/subsystems/nvme-target1
 cd /sys/kernel/config/nvmet/subsystems/nvme-target1
 
 #allow any host to connect to the target
-echo "allow any host to connect to the target"
 echo 1 > attr_allow_any_host
 
-echo "make new namespace 10"
-mkdir namespaces/10
+mkdir -p namespaces/10
 cd namespaces/10
 
-echo "setup remote nvme device"
-echo -n /dev/nvme0n1 > device_path
+echo -n /dev/nvme0n1 > $DEVICE_PATH
 echo 1 > enable
 
 #setting nvme port
-echo "setting remote nvme port"
-mkdir /sys/kernel/config/nvmet/ports/1
+mkdir -p /sys/kernel/config/nvmet/ports/1
 cd  /sys/kernel/config/nvmet/ports/1
 
-echo "setting other remote nvme configurations"
 echo ${IP_ADDR} > addr_traddr
 echo rdma > addr_trtype
 echo 4420 > addr_trsvcid
 echo ipv4 > addr_adrfam
 
 ln -s /sys/kernel/config/nvmet/subsystems/nvme-target1 /sys/kernel/config/nvmet/ports/1/subsystems/nvme-target1
-
-echo "Done!"
+echo "NVMe-oF over RDMA target setup done on ${IP_ADDR}"
