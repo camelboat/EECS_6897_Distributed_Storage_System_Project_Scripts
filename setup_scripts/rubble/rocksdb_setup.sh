bdf4f4c3e2515aaa019a9df9a6d479e57614ad3d
Changxu <cl3875@columbia.edu>
2021-04-04 12:47:15 -0600

    nit

--- setup_scripts/rubble/rocksdb_setup.sh
+++ setup_scripts/rubble/rocksdb_setup.sh
@@ -21,7 +21,7 @@ git clone https://gist.github.com/6e30397180d68b7e93969d63578fcc4c.git nlohmann_
 mv nlohmann_json/CMakeLists.txt /mnt/sdb/my_rocksdb/nlohmann_json/
 
 git clone https://gist.github.com/6fbdf9cca0ab96072f9959e5013b7aa5.git nlohmann_json_single_include
-mv nlohmann_json_single_include/json.hpp /mnt/sdb/my_rocksdb/single_include/
+mv nlohmann_json_single_include/json.hpp /mnt/sdb/my_rocksdb/nlohmann_json/single_include/
 
 # Install gRPC and protobuf
 cd /mnt/sdb/EECS_6897_Distributed_Storage_System_Project_Scripts/setup_scripts/gRPC
