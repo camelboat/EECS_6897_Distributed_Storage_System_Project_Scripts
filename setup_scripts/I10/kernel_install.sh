9ae52dbe3f3ac0b17d6579b668b5a7c5f2fda62e
Changxu <cl3875@columbia.edu>
2021-04-20 21:05:38 -0600

    Update kernel install script to allow choosing kernel version

--- setup_scripts/I10/kernel_install.sh
+++ setup_scripts/I10/kernel_install.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-set -e
+set -x
+
+sudo apt update
+sudo apt install build-essential libssl-dev libncurses-dev -y
+
+WORK_DIR='/mnt/sdb'
+# SRC_LINK='https://mirrors.edge.kernel.org/pub/linux/kernel/v4.x/linux-4.20.tar.gz'
+SRC_LINK='https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-5.0-rc3.tar.gz'
+PKG_NAME='linux-5.0-rc3'
 
 cd /mnt/sdb
-curl -OL https://mirrors.edge.kernel.org/pub/linux/kernel/v4.x/linux-4.20.tar.gz
+cd $WORK_DIR
+curl -OL $SRC_LINK
 
-# curl -OL https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-5.0-rc3.tar.gz
-tar -xvf linux-4.20.tar.gz
-cd linux-4.20
+tar -xvf ${PKG_NAME}.tar.gz
+cd ${PKG_NAME}
 
 cp /boot/config-4.15.0-137-generic .config
 yes "" | make oldconfig
 
-sudo apt-get install build-essential libssl-dev
+make menuconfig
 
 rm vmlinux-gdb.py
 rm -rf debian/
