16efaeca1e7551bae100823e76acee6841b0fcd0
cc4351 <cc4351@columbia.edu>
2021-05-29 15:23:39 -0600

    add save backup logic

--- test_scripts/rubble_rocksdb/rubble_cleanup.sh
+++ test_scripts/rubble_rocksdb/rubble_cleanup.sh
@@ -4,12 +4,17 @@
 primary="./primary_node"
 replica="./tail_node"
 COPY="False"
+BACKUP="False"
 # parse input
 for i in "$@"
 do
 case $i in
     -c=*|--copy=*)
     COPY="${i#*=}"
+    shift # past argument=value
+    ;;
+    -b=*|--backup=*)
+    BACKUP="${i#*=}"
     shift # past argument=value
     ;;
     --default)
@@ -21,22 +26,37 @@ case $i in
     ;;
 esac
 done
+
+prefix='/mnt/sdb/archive_dbs'
+# sleep for 60 seconds to let compaction finish
+# before saving a backup
+if [[ "$BACKUP" == "True" ]]; then
+    sleep 60
+    for role in "primary" "tail"
+    do
+        rsync -az "${prefix}"/"${role}"/db/ "${prefix}"/"${role}"/backup_db/
+        rsync -az "${prefix}"/"${role}"/sst_dir/*.sst "${prefix}"/"${role}"/backup_sst/
+    done
+fi
+
 # kill process
 kill $(ps aux | grep $replica | awk '{print $2}')
 kill $(ps aux | grep $primary | awk '{print $2}')
 # cleanup db files
-rm -rf /tmp/rubble_primary
-rm -rf /tmp/rubble_tail
-rm -rf /tmp/rubble_secondary
-rm -rf /tmp/rubble_vanilla
-rm -rf /mnt/sdb/archive_dbs/*/sst_dir/*.sst
+for role in "primary" "tail"
+do
+    rm -rf "${prefix}"/"${role}"/db
+    rm "${prefix}"/"${role}"/sst_dir/*.sst
+done
+
 # copy over new files
 if [[ "$COPY" == "True" ]]; then
     echo "COPY FILES OVER"
-    cp -r /tmp/backup_primary /tmp/rubble_primary
-    cp -r /tmp/backup_tail /tmp/rubble_tail
-    cp /mnt/sdb/archive_dbs/backup_primary/*.sst /mnt/sdb/archive_dbs/primary/sst_dir/
-    cp /mnt/sdb/archive_dbs/backup_tail/*.sst /mnt/sdb/archive_dbs/tail/sst_dir/
+    for role in "primary" "tail"
+    do
+        rsync -az "${prefix}"/"${role}"/backup_db/ "${prefix}"/"${role}"/db/ 
+        rsync -az "${prefix}"/"${role}"/backup_sst/*.sst "${prefix}"/"${role}"/sst_dir/
+    done
 fi
 # cleanup nohup.out log
 echo "--------rubble fresh start-------------" > /mnt/sdb/my_rocksdb/rubble/nohup.out
